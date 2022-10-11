cd7a5a695cecd9ebee94d05a6f1fa81aba9388e8
BuildEnough <rhtmaehcl878@naver.com>
2022-10-11 23:02:21 +0900

    어렵다고 생각한 문제 정리

--- 백준/어렵다고_생각한_문제/2480_주사위_세개.py
+++ 백준/어렵다고_생각한_문제/2480_주사위_세개.py
@@ -1,3 +1,5 @@
+# https://www.acmicpc.net/problem/2480
+
 dice = list(map(int, input().split()))
 
 a = 0
@@ -13,3 +15,11 @@ else:
         if dice.count(dice[i]) == 2:
             a = dice[dice.index(dice[i])]
     print(1000 + (a * 100))
+
+# set 중복 제거 이용
+# len(set(arr)) == 1이면 모든 수가 같은 수 => 같은 눈이 3개
+# len(set(arr)) == 3 이면 모두 다름
+
+# 2개일 경우
+# 반복문 + count + index
+# count가 2개 이상인 index찾아 dice[index]반환
