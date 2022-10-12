dd00063b7d88a660d7a43f3f7e1993dc3e8339cc
BuildEnough <rhtmaehcl878@naver.com>
2022-10-12 09:11:28 +0900

    수업 전 문제풀이

---
+++ 프로그래머스/레벨0/배열_뒤집기.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = num_list[::-1]
+    return answer
+
+
+def solution(num_list):
+    result = []
+    while num_list:
+        result.append(num_list.pop())
+    return result
---
+++ 프로그래머스/레벨0/세균_증식.py
@@ -0,0 +1,8 @@
+def solution(n, t):
+    for i in range(t):
+        n *= 2
+    return n
+
+
+def solution(n, t):
+    return n << t
