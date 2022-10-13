997defd1bc9cebeb4d56ec94711b821034cd7e3f
BuildEnough <rhtmaehcl878@naver.com>
2022-10-13 21:47:02 +0900

    알고리즘 문제

---
+++ 프로그래머스/레벨0/문자열_뒤집기.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    arr = [i for i in my_string]
+    arr.reverse()
+
+    answer = "".join(arr)
+    return answer
+
+
+def solution(my_string):
+    answer = my_string[::-1]
+    return answer
