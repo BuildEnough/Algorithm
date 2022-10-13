b79200db74246b2943c33ca1e98dc294ed7758c2
BuildEnough <rhtmaehcl878@naver.com>
2022-10-13 09:09:00 +0900

    수업전 문제 풀이

---
+++ 프로그래머스/레벨0/배열_원소의_길이.py
@@ -0,0 +1,14 @@
+def solution(strlist):
+    answer = []
+    for i in strlist:
+        answer.append(len(i))
+    return answer
+
+
+def solution(strlist):
+    answer = []
+    return [ len(i) for i in strlist ]
+
+
+def solution(strlist):
+    return [len(str) for str in strlist]
