94b0f0bfe2ddd4833260370dcaf8ea30af4547a8
BuildEnough <rhtmaehcl878@naver.com>
2022-11-30 22:27:24 +0900

    백준 알고리즘

---
+++ 백준/어렵다고_생각한_문제/덩치.py
@@ -0,0 +1,11 @@
+body = []
+for i in range(int(input())):
+    w, h = map(int, input().split())
+    body.append((w, h))
+
+for i in body:
+    a = 1
+    for j in body:
+        if i[0] < j[0] and i[1] < j[1]:
+            a += 1
+    print(a, end = ' ')
