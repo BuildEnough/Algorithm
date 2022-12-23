22240227b11f3c6d0c76f8a61af7f6f824d4e864
BuildEnough <rhtmaehcl878@naver.com>
2022-12-23 20:17:04 +0900

    백준알고리즘(틀린문제 수정)

--- 백준/어렵다고_생각한_문제/소수_구하기.py
+++ 백준/어렵다고_생각한_문제/소수_구하기.py
@@ -1,9 +1,8 @@
-N, M = map(int, input().split())
+m, n=map(int,input().split())
 
-for i in range(N, M+1):
-    if i == 0:
+for i in range(m, n+1):
+    if i == 1:
         continue
-
     for j in range(2, int(i ** 0.5) + 1):
         if i % j == 0:
             break
