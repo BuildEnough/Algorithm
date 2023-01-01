853b53def5a551266e9dc3b807cf0667b21d046d
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-01-01 23:41:10 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 12888 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int hour = scan.nextInt();
+        int minute = scan.nextInt();
+
+        minute -= 45;
+
+        if (minute < 0) {
+            hour -= 1;
+            minute += 60;
+            if (minute >= 60) {
+                hour -= 1;
+                minute -= 45;
+            }
+            if (hour < 0) {
+                hour = 23;
+            }
+        }
+        System.out.println(hour + " " + minute);
+
+    }
+}
