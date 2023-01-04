cc3fb3c809e24ebba2ebd2d1cd4d98ec7ea383eb
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-01-04 16:40:42 +0900

    [Bronze III] Title: 알람 시계, Time: 108 ms, Memory: 12892 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -11,10 +11,6 @@ public class Main {
         if (minute < 0) {
             hour -= 1;
             minute += 60;
-            if (minute >= 60) {
-                hour -= 1;
-                minute -= 45;
-            }
             if (hour < 0) {
                 hour = 23;
             }
