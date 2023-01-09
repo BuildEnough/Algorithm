c327d8bfc2cf50cef7a6af488b498ed34532959b
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-01-09 16:00:34 +0900

    [Bronze V] Title: 영수증, Time: 128 ms, Memory: 13060 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int X = scan.nextInt();
+        int N = scan.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < N; i++) {
+            int price = scan.nextInt();
+            int count = scan.nextInt();
+
+            sum += price * count;
+        }
+        if (X == sum)
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+
+    }
+}
