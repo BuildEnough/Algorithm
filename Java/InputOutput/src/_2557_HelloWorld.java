a2ceef935141fc11e2a938973b5febeef3b468e9
BuildEnough <rhtmaehcl878@naver.com>
2023-02-06 20:38:37 +0900

    백준 2557 방법2

--- Java/InputOutput/src/_2557_HelloWorld.java
+++ Java/InputOutput/src/_2557_HelloWorld.java
@@ -1,5 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 public class _2557_HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
     }
 }
