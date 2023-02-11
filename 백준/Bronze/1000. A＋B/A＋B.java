7f1968b93e7b4b6ba9b934e55f07b9f5121ac71b
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-11 13:29:18 +0900

    [Bronze V] Title: A+B, Time: 80 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,18 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// br.readline()를 통해 읽어온 것을 split("")하여 공백 단위로 나누어 준 뒤 String 배열에 각각 저장
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+        int A = Integer.parseInt(str[0]);
+        int B = Integer.parseInt(str[1]);
 
         System.out.println(A+B);
 
-        sc.close();
+
     }
 }
