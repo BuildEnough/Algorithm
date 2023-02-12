64d865fc7e21741f44dd9cd66dc7a14e15bd6aee
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-12 23:49:40 +0900

    [Bronze V] Title: A×B, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,11 +1,18 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int A = Integer.parseInt(st.nextToken()); // st.nextToken() 은 문자열을 반환하니 Integer.parseInt()로 int 형으로 변환
+        int B = Integer.parseInt(st.nextToken());
+
         System.out.println(A*B);
-        sc.close();
     }
 }
