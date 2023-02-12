1bb174109082f9be624d27fcd56e08add560e104
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-12 23:27:46 +0900

    [Bronze V] Title: A×B, Time: 108 ms, Memory: 12864 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int A = scan.nextInt();
-        int B = scan.nextInt();
-
-        System.out.println(A * B );
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A*B);
+        sc.close();
     }
 }
