578047ca6ec725684d9f6bc70ab35c7d96f5ae7e
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-13 14:24:11 +0900

    [Bronze V] Title: A/B, Time: 84 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,9 +1,17 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double A = sc.nextInt();
-        int B = sc.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+        double A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
         System.out.println(A/B);
     }
