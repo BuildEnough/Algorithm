9819a118f47e6b37ea580140deb80a726d900493
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-13 20:36:14 +0900

    [Bronze V] Title: 나머지, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // BufferedReader 는 문자열을 한 줄로 읽기 때문에 입력받은 문자열을 공백으로 분리
+        String str = br.readLine();
+
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
         System.out.println((A+B)%C);
         System.out.println(((A%C) + (B%C))%C);
         System.out.println((A*B)%C);
         System.out.println(((A%C) * (B%C))%C);
+
     }
 }
