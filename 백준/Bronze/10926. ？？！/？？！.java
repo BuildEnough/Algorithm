67e7449316e3a0f1e130e39cbb22e4ab053e188f
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-13 15:49:57 +0900

    [Bronze V] Title: ??!, Time: 84 ms, Memory: 11496 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,9 +1,14 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String name = scan.next();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        System.out.println(name + "??!");
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str + "??!");
+
     }
 }
