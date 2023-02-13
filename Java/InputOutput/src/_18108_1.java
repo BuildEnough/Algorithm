a151210454d5aa05c512bc85f5fc0797d8fcf38f
BuildEnough <rhtmaehcl878@naver.com>
2023-02-13 16:16:08 +0900

    18108

---
+++ Java/InputOutput/src/_18108_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class _18108_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int boolgi = sc.nextInt();
+        int sugi = 543;
+
+        System.out.println(boolgi - sugi);
+    }
+}
---
+++ Java/InputOutput/src/_18108_2.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _18108_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        int boolgi = Integer.parseInt(str);
+        int sugi = 543;
+
+        System.out.println(boolgi - sugi);
+    }
+}
