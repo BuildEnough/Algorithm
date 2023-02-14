dd57b538f5019a525ae6822a9fad35676195a3a2
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-14 14:17:15 +0900

    [Bronze V] Title: 시험 성적, Time: 76 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,18 +1,17 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int score = scan.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        if (score >= 90 && score <= 100)
-            System.out.println("A");
-        else if (score >= 80)
-            System.out.println("B");
-        else if (score >= 70)
-            System.out.println("C");
-        else if (score >= 60)
-            System.out.println("D");
-        else
-            System.out.println("F");
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int score = Integer.parseInt(br.readLine());
+
+        if (score >= 90) System.out.println('A');
+        else if (score >= 80) System.out.println('B');
+        else if (score >= 70) System.out.println('C');
+        else if (score >= 60) System.out.println('D');
+        else System.out.println('F');
     }
 }
