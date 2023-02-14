f1b839a29898fe81d7589768503834d336c70e10
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-14 22:46:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 80 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,17 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int hour = scan.nextInt();
-        int minute = scan.nextInt();
-        int oven = scan.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int min = 60 * hour + minute;
-        min += oven;
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(br.readLine());
 
-        hour = (min / 60) % 24;
-        minute = min % 60;
+        int min = A * 60 + B;
+        min += C;
 
-        System.out.println(hour + " " + minute);
+        A = (min / 60) % 24;
+        B = min % 60;
+
+
+        System.out.printf("%d %d", A, B);
     }
 }
