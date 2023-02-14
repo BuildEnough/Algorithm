36b097025e57c1e90c68211adf23f25562aae300
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-14 14:53:53 +0900

    [Bronze V] Title: 윤년, Time: 76 ms, Memory: 11500 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,11 +1,17 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int year = scan.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int year = Integer.parseInt(br.readLine());
+
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
             System.out.println(1);
-        else System.out.println(0);
+        else
+            System.out.println(0);
     }
 }
