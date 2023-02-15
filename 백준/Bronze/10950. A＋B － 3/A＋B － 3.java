ce1f6be0e9959974c33c1cb6c1ae79045e9ba46e
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-15 15:16:48 +0900

    [Bronze V] Title: A+B - 3, Time: 76 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,21 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = sc.nextInt();
-        int arr[] = new int[T];
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < T; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            arr[i] = A + B;
+            st = new StringTokenizer(br.readLine());
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append('\n');
         }
-        sc.close();
-
-        for (int j : arr) {
-            System.out.println(j);
-        }
+        System.out.println(sb);
     }
 }
