5ce63fdc938fe177de14182c679d684c71460cfb
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-15 11:44:41 +0900

    [Bronze V] Title: 구구단, Time: 76 ms, Memory: 11396 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,11 +1,20 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt();
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = 1; i < 10; i++) {
-            System.out.println(N + " * " + i + " = " + N*i);
+            sb.append(N).append(' ').append('*').append(' ').append(i);
+            sb.append(' ').append('=').append(' ').append(N*i).append('\n');
         }
+        System.out.println(sb);
     }
 }
