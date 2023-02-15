3455f59d3b2f43aca15641caadef399cae06fe31
BuildEnough <rhtmaehcl878@naver.com>
2023-02-15 14:16:15 +0900

    2739

---
+++ Java/ConditionalStatement/src/_2739_3.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _2739_3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < 10; i++) {
+            sb.append(N).append(' ').append('*').append(' ').append(i);
+            sb.append(' ').append('=').append(' ').append(N*i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
