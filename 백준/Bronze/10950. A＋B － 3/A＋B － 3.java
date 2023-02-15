cecb3c0e4678d954827e4581d1c56ce37b18330b
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-15 14:37:41 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 13100 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int loop = scan.nextInt();
-
-        for (int i = 0; i < loop; i++) {
-            int A = scan.nextInt();
-            int B = scan.nextInt();
-
+        int T = sc.nextInt();
+        
+        for (int i = 0; i < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
             System.out.println(A+B);
         }
+        sc.close();
     }
 }
