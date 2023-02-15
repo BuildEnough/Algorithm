65ff6f7c60cf8b9c0896933462a9de84e6c438ce
BuildEnough <rhtmaehcl878@naver.com>
2023-02-15 14:26:07 +0900

    2739

---
+++ Java/ConditionalStatement/src/_2739_4.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class _2739_4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        for (int i = 1; i < 10; i++) {
+            bw.write(N+" * "+i+" = "+N*i+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
