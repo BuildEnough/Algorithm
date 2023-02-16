dd45dc3de79e1f7302f14bc5ec6bb460d8f7fee0
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-16 11:36:41 +0900

    [Bronze V] Title: 합, Time: 76 ms, Memory: 11412 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,12 +1,17 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        int n = scan.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        br.close();
         int sum = 0;
 
-        for (int i = 1; i < n+1; i++) {
+
+        for (int i = 0; i <= n; i++) {
             sum += i;
         }
         System.out.println(sum);
