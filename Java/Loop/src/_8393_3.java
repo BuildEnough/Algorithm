ef5da9a2a2996de8915a2bfe2b585d10ccfd3d75
BuildEnough <rhtmaehcl878@naver.com>
2023-02-16 14:30:19 +0900

    8393

---
+++ Java/Loop/src/_8393_3.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _8393_3 {
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        int sum = 0;
+
+
+        for (int i = 0; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
