e4909b4dc0e577c9a1b3528dcf2ef7b274266a23
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-20 15:09:54 +0900

    [Bronze V] Title: A+B - 8, Time: 80 ms, Memory: 11708 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int T = Integer.parseInt(br.readLine());
 
@@ -16,9 +19,13 @@ public class Main {
             st = new StringTokenizer(br.readLine(), " ");
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            sb.append("Case #" + i + ": ").append(A + " + " + B + " = " + (A+B)).append('\n');
+
+            bw.write("Case #"+i+": ");
+            bw.write(A + " + " + B + " = ");
+            bw.write((A+B) + "\n");
         }
         br.close();
-        System.out.println(sb);
+        bw.flush();
+        bw.close();
     }
 }
