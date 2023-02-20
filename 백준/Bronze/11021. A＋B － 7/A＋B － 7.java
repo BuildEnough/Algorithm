fec0548eded9fe55a9b4fc44764ac57593450011
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-20 14:23:04 +0900

    [Bronze V] Title: A+B - 7, Time: 76 ms, Memory: 11484 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -1,26 +1,20 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= T; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            bw.write("Case #"+i+": ");
-            bw.write(Integer.parseInt(st.nextToken())
-                    +Integer.parseInt(st.nextToken())+"\n");
+            String str = br.readLine();
+            sb.append("Case #").append(i).append(": ")
+                    .append(str.charAt(0)-'0'+str.charAt(2)-'0').append('\n');
         }
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
     }
 }
