66457744a72d8faa4b44d65ff3538310adeb1825
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-21 09:42:18 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 12896 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,10 +1,13 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        int N = sc.nextInt();
-        sc.close();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        br.close();
 
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
