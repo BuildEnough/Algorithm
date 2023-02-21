09add98a6afd0a7cab47c586a9ccee61f3f7b9b0
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-21 10:13:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 236 ms, Memory: 13340 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,17 +1,18 @@
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-        int n = scan.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n-i; j++) {
-                System.out.print(" ");
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N-i; j++) {
+                System.out.print(' ');
             }
-            for (int k = 0; k < i; k++) {
-                System.out.print("*");
+            for (int j = 1; j <= i; j++) {
+                System.out.print('*');
             }
             System.out.println();
         }
