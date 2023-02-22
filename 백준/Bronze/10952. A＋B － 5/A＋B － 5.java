766632ba45510955a803c5e5d73671d44990d662
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-22 10:09:33 +0900

    [Bronze V] Title: A+B - 5, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,19 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         while (1>0) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-
+            String str = br.readLine();
+            int A = str.charAt(0)-'0';
+            // int A = str.charAt(0) - 48;
+            // int A = str.charAt(0) - 48;
+            
+            int B = str.charAt(2)-'0';
+            // int B = str.charAt(2) - 48;
+            
             if (A==0 && B==0)
                 break;
             else
