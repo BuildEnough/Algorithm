2870ccb2d78f733e9e8298b27471d073f3d08660
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-22 09:26:19 +0900

    [Bronze V] Title: A+B - 5, Time: 128 ms, Memory: 13080 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        while (1 == 1) {
+        while (1>0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
 
-            int A = scan.nextInt();
-            int B = scan.nextInt();
-
-            if (A == 0 && B == 0){
+            if (A==0 && B==0)
                 break;
-            }
-            System.out.println(A+B);
+            else
+                System.out.println(A+B);
         }
     }
 }
