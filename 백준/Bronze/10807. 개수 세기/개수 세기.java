93a914517923ebe07e4774e81de2686caaa921a2
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-24 15:49:50 +0900

    [Bronze V] Title: 개수 세기, Time: 116 ms, Memory: 12984 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int N = scan.nextInt(); // 정수 개수 입력
+        Scanner sc = new Scanner(System.in);
 
-        int[] arr = new int[N]; // 배열 만들기
-        int sycle = 0; // 사이클 횟수
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int count = 0;
 
         for (int i = 0; i < N; i++) {
-            arr[i] = scan.nextInt();
+            arr[i] = sc.nextInt();
         }
 
-        int v = scan.nextInt(); // 찾으려고 하는 정수
+        int find = sc.nextInt();
 
         for (int i = 0; i < arr.length; i++) {
-            if (v == arr[i])
-                sycle += 1;
+            if (find == arr[i])
+                count += 1;
         }
-        System.out.println(sycle);
+        System.out.println(count);
     }
 }
