e87fa3811ae682c4a1d78c689af8d46c8dbcd010
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-25 10:38:55 +0900

    [Bronze V] Title: 개수 세기, Time: 80 ms, Memory: 11576 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,21 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
-        int count = 0;
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i <arr.length; i++) {
+            arr[i] += Integer.parseInt(st.nextToken());
         }
 
-        int find = sc.nextInt();
+        int find = Integer.parseInt(br.readLine());
 
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (find == arr[i])
-                count += 1;
+            if (arr[i] == find)
+                count++;
         }
         System.out.println(count);
     }
