8eab776c2e19603c6f12f15994ee0b8b9e91c7af
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-26 19:09:01 +0900

    [Bronze V] Title: X보다 작은 수, Time: 276 ms, Memory: 25972 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int N = scan.nextInt(); // 배열의 수
-        int X = scan.nextInt(); // X보다 작은 정수를 구해야함
+        int N = sc.nextInt();
+        int X = sc.nextInt();
 
-        int arr[] = new int[N]; // N개 크기의 배열
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            arr[i] = scan.nextInt(); // N개 크기의 배열에 입력
+            int bi = sc.nextInt();
+            if (bi < X)
+                sb.append(bi + " ");
         }
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < X) // 배열의 각 값이 X보다 작을 때
-                System.out.print(arr[i] + " ");
-        }
+        sc.close();
+        System.out.println(sb);
 
     }
 }
