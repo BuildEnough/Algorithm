7ec1044c21fcf6d536b178bf01aeadc9e5444af6
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-28 23:06:41 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 76 ms, Memory: 11356 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,12 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[31];
 
         for (int i = 1; i <= 28; i++) {
-            int check = sc.nextInt();
+            int check = Integer.parseInt(br.readLine());
             arr[check] = 1;
 
         }
