6b57008b87c8db820e8a4ab91d8544d39ad8c42e
BuildEnough <89980923+BuildEnough@users.noreply.github.com>
2023-02-28 20:49:37 +0900

    [Bronze II] Title: 공 바꾸기, Time: 80 ms, Memory: 11816 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -1,10 +1,15 @@
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N];
 
@@ -14,13 +19,15 @@ public class Main {
 
         for (int i = 0; i < M; i++) {
             int tmp;
-            int F = sc.nextInt();
-            int E = sc.nextInt();
+            st = new StringTokenizer(br.readLine(), " ");
+            int F = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
             tmp = arr[F-1];
             arr[F-1] = arr[E-1];
             arr[E-1] = tmp;
         }
+        br.close();
 
         for (int baguni : arr)
             System.out.print(baguni + " ");
