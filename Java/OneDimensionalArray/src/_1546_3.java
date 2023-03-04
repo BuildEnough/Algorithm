9a0b48d7beab20445aeb2aae07959955172f208c
BuildEnough <rhtmaehcl878@naver.com>
2023-03-04 11:23:03 +0900

    1546

--- Java/OneDimensionalArray/src/_1546_3.java
+++ Java/OneDimensionalArray/src/_1546_3.java
@@ -8,14 +8,14 @@ public class _1546_3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        double[] arr = new double[Integer.parseInt(br.readLine())];
+        int N = Integer.parseInt(br.readLine());
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
         int max = -1; // -1: 입력받을 값이 0보다 큼
         double sum = 0.0;
 
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < N; i++) {
             int value = Integer.parseInt(st.nextToken());
 
             if(value > max) {
@@ -24,6 +24,6 @@ public class _1546_3 {
             sum += value;
         }
 
-        System.out.println(((sum / max) * 100.0) / arr.length);
+        System.out.println(((sum / max) * 100.0) / N);
     }
 }
