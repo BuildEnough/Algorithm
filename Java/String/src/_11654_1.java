85d9aa1d23a619bfbd035cf6643825f216b5d7e4
BuildEnough <rhtmaehcl878@naver.com>
2023-03-12 19:41:28 +0900

    11654

---
+++ Java/String/src/_11654_1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _11654_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int c = sc.next().charAt(0);
+
+        System.out.println(c);
+    }
+}
